2898885cd5920d94b2ab7af63f1a2891e75cff46
hillarynda <hillarynda0@gmail.com>
2023-11-20 19:49:48 -0300

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Apple Dev on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
